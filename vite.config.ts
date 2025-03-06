105ab5c88d1499d99ea8bdcd5364c6754c27ce60
RodXOTWOD <rodxotwod@gmail.com>
2025-03-06 22:29:18 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,6 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/foreverticket-landing",
+  base: "/foreverticket-landing/",
 
 }));
